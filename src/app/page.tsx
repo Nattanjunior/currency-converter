b06da384e5abd1611d263280142aeb4e430e4c48
nattanjunior <fnatanieljunior@gmail.com>
2024-08-06 20:14:54 -0300

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
+import { Inputs } from "@/components/inputs";
+
 
 export default function Home() {
   return (
-   <></>
+    <>
+      <main className="bg-black w-full h-screen grid place-items-center">
+        <div className="bg-white flex flex-col gap-[6.4rem] items-center w-[80rem] h-[69.2rem] rounded-2xl py-14 max-md:w-[70%]">
+          <Inputs />
+          {/* <div className="bg-red-600">çklwdmvklamsklfdm</div> */}
+        </div>
+      </main>
+    </>
   );
 }
