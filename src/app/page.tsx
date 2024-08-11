9cab3e2e1ba532ea9f1e1542f2d36c150e5c6545
nattanjunior <fnatanieljunior@gmail.com>
2024-08-10 21:06:35 -0300

    update: taking information from the input and creating the formatting of the input value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <main className="bg-black w-full h-screen grid place-items-center">
-        <div className="bg-white flex flex-col gap-[6.4rem] items-center w-[80rem] h-[69.2rem] rounded-2xl py-14 max-md:w-[70%]">
+        <div className="bg-white flex flex-col gap-[6.4rem] items-center w-[80rem] h-[69.2rem] rounded-2xl py-14 max-md:w-[90%]">
           <Inputs />
           {/* <div className="bg-red-600">çklwdmvklamsklfdm</div> */}
         </div>
