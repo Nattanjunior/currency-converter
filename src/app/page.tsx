0a990713712d9a108a1ff2ea0c23830a3e999121
nattanjunior <fnatanieljunior@gmail.com>
2024-08-18 20:33:09 -0300

    estilization div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import { Inputs } from "@/components/inputs";
+import ApexChart from "@/components/charts";
+import  Inputs from "@/components/inputs";
+
 
 
 export default function Home() {
   return (
     <>
       <main className="bg-black w-full h-screen grid place-items-center">
-        <div className="bg-white flex flex-col gap-[6.4rem] items-center w-[80rem] h-[69.2rem] rounded-2xl py-14 max-md:w-[90%]">
+        <div className="bg-white flex flex-col gap-[6.4rem] max-md:gap-10 items-center w-[80rem] rounded-2xl py-32 max-md:w-[33rem] max-md:rounded-lg"> 
           <Inputs />
-          {/* <div className="bg-red-600">çklwdmvklamsklfdm</div> */}
+          <ApexChart />
         </div>
       </main>
     </>
