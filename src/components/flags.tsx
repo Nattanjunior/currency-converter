c9e903f5b822ea5b163a43cf738b940acd6accda
nattanjunior <fnatanieljunior@gmail.com>
2024-08-18 20:36:40 -0300

    create: created component of flags coins

---
+++ src/components/flags.tsx
@@ -0,0 +1,34 @@
+import type { HandleProps } from "./inputs"
+
+type Props = {
+  ValueSelected: (value : HandleProps) => void
+}
+
+export function Flags({ValueSelected} : Props) {
+
+  function handleSelected({ flag, name }: HandleProps) {
+    ValueSelected({flag, name})
+  }
+  return (
+    <>
+      <ul className="bg-white mt-[.8rem] shadow-black border-2 flex flex-col gap-3 list-none w-[16rem] rounded-[.8rem]">
+        <li onClick={() => handleSelected({ flag: 'fi fi-br fis', name: 'BRL' })}>
+          <span className="fi fi-br fis"></span>
+          BRL
+        </li>
+        <li onClick={() => handleSelected({ flag: 'fi fi-us fis', name: 'USD' })}>
+          <span className="fi fi-us fis"></span>
+          USD
+        </li>
+        <li onClick={() => handleSelected({ flag: 'fi fi-eu fis', name: 'EUR' })}>
+          <span className="fi fi-eu fis"></span>
+          EUR
+        </li>
+        <li onClick={() => handleSelected({ flag: 'fi fi-gb fis', name: 'GBP' })}>
+          <span className="fi fi-gb fis"></span>
+          GBP
+        </li>
+      </ul>
+    </>
+  )
+}
