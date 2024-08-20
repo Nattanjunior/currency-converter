38ab58066b5927f11f12aa3a564e8a294837d722
nattanjunior <fnatanieljunior@gmail.com>
2024-08-20 20:28:30 -0300

    call of API failed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <main className="bg-black w-full h-screen grid place-items-center">
-        <div className="bg-white flex flex-col gap-[6.4rem] max-md:gap-10 items-center w-[80rem] rounded-2xl py-32 max-md:w-[33rem] max-md:rounded-lg"> 
+        <div className="bg-white flex flex-col gap-[6.4rem] max-md:gap-10 items-center w-[80rem] rounded-2xl py-32 max-md:pt-14 max-md:w-[33rem] max-md:rounded-[1.6rem]"> 
           <Inputs />
           <ApexChart />
         </div>
